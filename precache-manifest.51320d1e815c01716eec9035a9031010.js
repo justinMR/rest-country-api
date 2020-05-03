55cea450984e9cb7f5c3ef69ed650e1c15cb3ef2
Justin Michael Ramirez <JustinMichaelRamirez@gmail.com>
2020-05-03 19:06:16 -0400

    Updates

---
+++ precache-manifest.51320d1e815c01716eec9035a9031010.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78e1265829dc1b75cba2955a59f57be5",
+    "url": "/rest-country-api/index.html"
+  },
+  {
+    "revision": "205104b145cb9aedab82",
+    "url": "/rest-country-api/static/css/2.cd1899df.chunk.css"
+  },
+  {
+    "revision": "db58b4c17d61b88b83b1",
+    "url": "/rest-country-api/static/css/main.00e59506.chunk.css"
+  },
+  {
+    "revision": "205104b145cb9aedab82",
+    "url": "/rest-country-api/static/js/2.df849ad8.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/rest-country-api/static/js/2.df849ad8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "db58b4c17d61b88b83b1",
+    "url": "/rest-country-api/static/js/main.a80e6725.chunk.js"
+  },
+  {
+    "revision": "4a0022247c7ea293b8ec",
+    "url": "/rest-country-api/static/js/runtime-main.e553e174.js"
+  }
+]);
